fa04778f3a26113a44dd29d4d571d2612f90bd04
Ubaydullo Asatullaev <Ubaydullohazard@gmail.com>
2024-03-21 11:07:47 +0900

    Update loops.py

--- Python/loops.py
+++ Python/loops.py
@@ -4,7 +4,10 @@ if __name__ == '__main__':
     for i in range(n):
         print(i*i)
 
-      
+
+# Task
+# The provided code stub reads and integer, n, from STDIN. For all non-negative integers i < n, print i2 . 
+
 
 # Test Input:
 # 5
